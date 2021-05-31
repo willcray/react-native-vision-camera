24bcd6c1d3090255da284330fecb0f32fe18552c
Marc Rousavy <marcrousavy@hotmail.com>
2021-05-31 13:30:52 +0200

    yea

--- example/ios/Frame Processor Plugins/QR Code Plugin (Objective-C)/QRCodeFrameProcessorPluginObjC.m
+++ example/ios/Frame Processor Plugins/QR Code Plugin (Objective-C)/QRCodeFrameProcessorPluginObjC.m
@@ -7,6 +7,8 @@
 
 #import <Foundation/Foundation.h>
 #import <VisionCamera/FrameProcessorPlugin.h>
+#import <MLKitVision/MLKitVision.h>
+#import <MLKitFaceDetection/MLKitFaceDetection.h>
 
 // Example for an Objective-C Frame Processor plugin
 
@@ -15,20 +17,33 @@
 
 @implementation QRCodeFrameProcessorPluginObjC
 
-+ (MLKFaceDetectorOptions*)options {
-  static MLKFaceDetectorOptions* options;
-  if (options == nil) {
-    options = [[MLKFaceDetectorOptions alloc] init];
-    options.performanceMode = MLKFaceDetectorPerformanceModeAccurate;
++ (MLKFaceDetector*)faceDetector {
+  static MLKFaceDetector* faceDetector;
+  if (faceDetector == nil) {
+    MLKFaceDetectorOptions* options = [[MLKFaceDetectorOptions alloc] init];
     options.landmarkMode = MLKFaceDetectorLandmarkModeAll;
     options.classificationMode = MLKFaceDetectorClassificationModeAll;
+    faceDetector = [MLKFaceDetector faceDetectorWithOptions:options];
   }
-  return options;
+  return faceDetector;
 }
 
 static inline id exampleObjC___scanQRCodes(CMSampleBufferRef buffer, NSArray* arguments) {
-  // TODO: Use some AI to detect QR codes in the CMSampleBufferRef
-  return @[];
+  MLKVisionImage *image = [[MLKVisionImage alloc] initWithBuffer:buffer];
+  image.orientation = UIImageOrientationUp;
+  NSArray<MLKFace*>* faces = [[QRCodeFrameProcessorPluginObjC faceDetector] resultsInImage:image error:nil];
+  
+  NSMutableArray* results = [[NSMutableArray alloc] initWithCapacity:faces.count];
+  for (MLKFace* face in faces) {
+    [results addObject:@{
+      @"x": @(face.frame.origin.x),
+      @"y": @(face.frame.origin.y),
+      @"height": @(face.frame.size.height),
+      @"width": @(face.frame.size.width),
+    }];
+  }
+  
+  return results;
 }
 
 VISION_EXPORT_FRAME_PROCESSOR(exampleObjC___scanQRCodes)
